0ba7aec9986b2922f85819a8eaf389bbab3c46fe
Rojikku <Rojikku@users.noreply.github.com>
2023-04-28 19:46:48 -0400

    src: Remove old scripts

--- src/kill-all.js
+++
@@ -1,7 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-    let c2 = ns.ls("home", "c2/");
-    for (let server of c2) {
-        ns.rm(server);
-    }
-}
--- src/test.js
+++
@@ -1,6 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-    let levelGoal = 10;
-    ns.tprint(ns.getHackingLevel());
-    ns.tprint(parseInt(levelGoal));
-}
